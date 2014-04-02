7fdd33a9fd55bdbfa5d6bc55c2e569750507187a
Håkon Rønning <hakonsr@stud.ntnu.no>
2014-04-02 14:48:19 +0200

    project reboot

---
+++ Assets/cameraMotion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMotion : MonoBehaviour {
+	private Compass compass;
+	private int yaw = 0;
+	private int dif = 0;
+	private int absDif = 0;
+	private int oldYaw = 0;
+	// Use this for initialization
+	void Start () {
+		// Enable the compass.
+		Input.location.Start();
+		compass = Input.compass;
+		compass.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		yaw = (int)compass.trueHeading;
+		dif = yaw-oldYaw;
+		absDif =  Mathf.Abs(yaw-oldYaw);
+		if(absDif > 10){
+			//transform.rotation = Quteranion.Euler(0, yaw, 0);
+			transform.Rotate(0,dif,0);
+			oldYaw = yaw;
+		}
+	}
+
+	void OnGUI() {
+		if (compass != null)
+			GUI.Label(new Rect(10, Screen.height / 2 + 100, 500, 100),
+			          "heading: " + compass.trueHeading);
+	}
+}
---
+++ Assets/mouseMotion.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseMotion : MonoBehaviour {
+	//private double sens = 0.05;
+	//private Gyroscope gyro;
+	private float sensitivity = 2f;
+	// Use this for initialization
+	void Start () {
+		//gyro = Input.gyro; // Store the reference for Gyroscope sensor
+		//gyro.enabled = true; //Enable the Gyroscope sensor
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Mouse Y") != 0){
+			transform.Rotate(-Input.GetAxis("Mouse Y")*sensitivity, 0, 0, Space.Self);
+
+		}
+		if(Input.GetAxis("Mouse X") != 0){
+			transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivity, 0, Space.World);
+			
+		}
+
+		/*if(gyro.rotationRateUnbiased.x > sens | gyro.rotationRateUnbiased.x < -sens){
+			transform.Rotate(-gyro.rotationRateUnbiased.x, -gyro.rotationRateUnbiased.y, gyro.rotationRateUnbiased.z);
+		}
+		else if(gyro.rotationRateUnbiased.y > sens | gyro.rotationRateUnbiased.y < -sens){
+			transform.Rotate(-gyro.rotationRateUnbiased.x, -gyro.rotationRateUnbiased.y, gyro.rotationRateUnbiased.z);
+		}
+		else if(gyro.rotationRateUnbiased.z > sens | gyro.rotationRateUnbiased.z < -sens){
+			transform.Rotate(-gyro.rotationRateUnbiased.x, -gyro.rotationRateUnbiased.y, gyro.rotationRateUnbiased.z);
+		}
+*/
+	}
+}
